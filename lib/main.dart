bfad0e9f83d1c01d689b47f18be2d415ebab4fd6
Pol de Visser <p.devisser@bxter.nl>
2023-06-07 08:24:39 +0200

    cleanup after break

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,6 @@ import 'package:draw_app/widgets/selection%20wheel/selection_wheel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'widgets/collapsible menu/collapsible_menu.dart';
-import 'widgets/collapsible menu/collapsible_menu_item.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
@@ -43,11 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        const DrawableCanvas(),
-        CollapsibleMenu(items: menuItems),
-        const SelectionWheel(),
-      ]),
+      body: Stack(children: const [DrawableCanvas(), SelectionWheel()]),
     );
   }
 }
